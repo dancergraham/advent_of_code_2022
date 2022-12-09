229049f55e6413f021668db2db614f7175583cfd
Graham Knapp <graham.knapp@gmail.com>
2022-12-09 06:27:20 +0100

    wip: day 6 in rust

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,15 +1,38 @@
 use std::collections::HashSet;
+use std::fs::File;
 
 #[cfg(test)]
 mod tests {
-    static TEST_INPUT: &str = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+    static TEST_INPUT: &str = "$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k";
 
     #[test]
     fn test_part_1() {
         use super::*;
         let answer = part_1(TEST_INPUT);
         println!("The test answer to Part 1 is {}", answer);
-        assert_eq!(answer, 7);
+        assert_eq!(answer, 95437);
     }
 
     #[test]
@@ -42,13 +65,56 @@ fn main() {
         }
     }
 }
+struct File_obj<'a> {
+    name: &'a str,
+    size: usize,
+    contents: Vec<File_obj<'a>>,
+    is_dir: bool,
+}
 
 fn part_1(input: &str) -> u32 {
+    use slab_tree::*;
     let mut answer = 0;
-    for (i, vec) in input.as_bytes().windows(4).enumerate() {
-        if vec.len() == vec.into_iter().collect::<HashSet<_>>().len() {
-            answer = i + 4;
-            break;
+    let mut cd = File_obj {
+        name: &"/",
+        size: 0,
+        contents: Vec::new(),
+        is_dir: true,
+    };
+    let mut path = vec![&cd];
+    for line in input.lines() {
+        if line.chars().next().expect("no blank lines") == '$' {
+            line.chars().next();
+            let commands: Vec<&str> = line.split(' ').collect();
+            if commands[1] == "ls" {
+                continue;
+            } else if commands[1] == "cd" {
+                if commands[2] == "/" {
+                    continue;
+                } else if commands[2] == ".." {
+                    let index = path.len();
+                    path.remove(index);
+                }
+            }
+            println!("{}", commands[1])
+        } else {
+            let values: Vec<&str> = line.split(' ').collect();
+            if values[0].parse::<usize>().is_ok() {
+                cd.contents.push(File_obj {
+                    name: values[1],
+                    size: values[0].parse::<usize>().expect("OK"),
+                    contents: vec![],
+                    is_dir: false,
+                })
+            } else {
+                cd.contents.push(File_obj {
+                    name: values[1],
+                    size: 0,
+                    contents: vec![],
+                    is_dir: true,
+                })
+            }
+            println!("{}", line)
         }
     }
     return answer as u32;
